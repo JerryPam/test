235e0f39ab73eb90695dddf577e0747ec975ae03
vavilon <vm128@yandex.ru>
2020-08-19 00:01:07 +0300

    HW3 recovering Color once more

--- src/HomeWork3/LookColor.java
+++ src/HomeWork3/LookColor.java
@@ -1,4 +1,15 @@
+/*
+3. В отдельном классе, создать мэйн метод. Внутри данного метода создать экземпляр Color.
+В конструктор передать число 3. Далее вывести в консоль номер цвета и его названия
+используя результат выполнения методов getNumber и getName.
+ */
+
 package HomeWork3;
 
 public class LookColor {
+    public static void main(String[] args) {
+        Color A = new Color(3);
+        System.out.println(A.getNumber());
+        System.out.println(A.getName());
+    }
 }
